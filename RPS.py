1fb146628348bc59104d8f774b04c9a4b955cc27
Ankhbayar Dulmaa <ankhbayar.ed@gmail.com>
2025-03-02 16:31:10 +0000

    Updated RPS.py and main.py with improvements

--- RPS.py
+++ RPS.py
@@ -1,10 +1,31 @@
-# The example function below keeps track of the opponent's history and plays whatever the opponent played two plays ago. It is not a very good player so you will need to change the code to pass the challenge.
+import random
 
 def player(prev_play, opponent_history=[]):
-    opponent_history.append(prev_play)
+    if prev_play:
+        opponent_history.append(prev_play)
 
-    guess = "R"
-    if len(opponent_history) > 2:
-        guess = opponent_history[-2]
+    pattern_count = {}
+    pattern_length = 3  
 
-    return guess
+
+    if len(opponent_history) >= pattern_length:
+        for i in range(len(opponent_history) - pattern_length):
+            pattern = tuple(opponent_history[i:i + pattern_length])
+            next_move = opponent_history[i + pattern_length]
+
+            if pattern not in pattern_count:
+                pattern_count[pattern] = {"R": 0, "P": 0, "S": 0}
+            pattern_count[pattern][next_move] += 1
+
+
+        last_pattern = tuple(opponent_history[-pattern_length:])
+        if last_pattern in pattern_count:
+            predicted_move = max(pattern_count[last_pattern], key=pattern_count[last_pattern].get)
+        else:
+            predicted_move = random.choice(["R", "P", "S"])
+    else:
+        predicted_move = random.choice(["R", "P", "S"])
+
+
+    counter_move = {"R": "P", "P": "S", "S": "R"}
+    return counter_move[predicted_move]
